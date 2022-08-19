dfea5c0d80f3ede5bc271be67c52701ee8b0792d
JaeHyeon Lee <project0858@gmail.com>
2022-08-19 17:43:39 +0900

    HI

---
+++ src/test/TestExample.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TestExample {
+
+	public static void main(String[] args) {
+		
+	}
+	static void mine() {
+		
+	}
+}
